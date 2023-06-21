f21236e2e9208001b68d0872ce49b25316a92e22
Ruslan Pashayev <pashayevrn@gmail.com>
2023-06-21 18:28:56 -0500

    initial commit

---
+++ flask_app/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask, session
+from flask_bcrypt import Bcrypt
+import os
+
+app = Flask(__name__)
+app.secret_key = os.environ.get('FLASK_APP_SECRET_KEY')
+
+app.config['UPLOAD_FOLDER'] = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'static/assets/upload')
+app.config['AVATAR_FOLDER'] = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'static/assets/avatars')
+app.config['TEMPLATES_FOLDER'] = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'templates')
+
+bcrypt = Bcrypt(app)
---
+++ server.py
@@ -0,0 +1,6 @@
+from flask_app import app
+from flask_app.controllers import c1, c2
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

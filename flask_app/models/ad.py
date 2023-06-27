ae5f6e6dd1726e086b51b5551b1325ba9c4ab847
Ruslan Pashayev <92914820+rpashayev@users.noreply.github.com>
2023-06-27 08:39:01 -0500

    Update ad.py

--- flask_app/models/ad.py
+++ flask_app/models/ad.py
@@ -3,6 +3,8 @@ from flask_app import app
 from flask_app.config.mysqlconnection import connectToMySQL
 from flask_app.models import category, message, user, vendor
 
+AD_REGEX = re.compile(r'^\s*$')
+
 class Ad:
     DB = 'wedding_vendors_schema'
     def __init__(self, data):
@@ -15,6 +17,22 @@ class Ad:
         self.category = None
         self.image = None
 
+    @classmethod
+    def category_ad(cls, data):
+        all_ads = []
+        query = '''
+            SELECT *
+            FROM ads
+            WHERE ads.category_id = %(category_id)s;
+        '''
+        results = connectToMySQL(cls.DB).query_db(query, data)
+        if len(results) < 1:
+            return False
+            
+        for result in results:
+            all_ads.append(cls(result))
+        return all_ads
+    
     @classmethod
     def delete_ad(cls, data):
         query = '''
@@ -42,3 +60,19 @@ class Ad:
             WHERE ads.id = %(ad_id)s;
         '''
         return connectToMySQL(cls.DB).query_db(query, data)
+
+
+    @staticmethod
+    def validate_ad(ad):
+        isValid = True
+        if Ad.category_ad(ad):
+            flash('You already have ad from this category', 'ad_error')
+            is_valid = False
+            return isValid
+        if len(ad['ad_content']) < 3 or AD_REGEX.match(ad['ad_content']):
+            flash('Ad content cannot be empty', 'ad_error')
+            is_valid = False
+        if not ad['image_id']:
+            flash('Please select the image for your ad', 'ad_error')
+            is_valid = False
+        return isValid

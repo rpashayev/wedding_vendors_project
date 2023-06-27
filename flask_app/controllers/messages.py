ad59d54a14d0d90cbea04d05fedfcf408fbd39cf
Ruslan Pashayev <pashayevrn@gmail.com>
2023-06-26 21:57:50 -0500

    wip on messages

--- flask_app/controllers/messages.py
+++ flask_app/controllers/messages.py
@@ -11,3 +11,18 @@ def send_message():
         
     message.Message.send_message(request.form)
     return redirect(f'/vendors/view/{request.form["receiver_id"]}')
+
+@app.route('/messages/get', methods=['POST'])
+def get_messages():
+    session['cp_id'] = request.form['cp_id']
+    
+    return redirect('/messages/view')
+
+@app.route('/messages/view')
+def view_messages():
+    data = {
+        'cp_id': session['cp_id'],
+        'current_id': session['id']
+    }
+    
+    return render_template('test_view_messages.html', msgs = message.Message.get_user_msg_exchange(data))

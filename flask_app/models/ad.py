801d56b91298d8e2d3a289c0d7e4d547d2a43fd1
Ruslan Pashayev <92914820+rpashayev@users.noreply.github.com>
2023-06-27 08:09:50 -0500

    Update ad.py

--- flask_app/models/ad.py
+++ flask_app/models/ad.py
@@ -16,11 +16,29 @@ class Ad:
         self.image = None
 
     @classmethod
-    def delete_add(cls, data):
+    def delete_ad(cls, data):
         query = '''
             DELETE
             FROM ads
             WHERE ads.id = %(ad_id)s;
         
+        '''
+        return connectToMySQL(cls.DB).query_db(query, data)
+
+    @classmethod
+    def new_ad(cls, data):
+        query = '''
+            INSERT
+            INTO ads(ad_content, category, image, vendor)
+            VALUES %(ad_content)s, %(category_ids, %(image_id)s, %(vendor_id)s ;
+        '''
+        return connectToMySQL(cls.DB).query_db(query, data)
+
+    @classmethod
+    def edit_ad(cls, data):
+        query = '''
+            UPDATE ads
+            SET ad_content=%(ad_content)s, category = %(category_ids)s, image = %(image_id)s, vendor = %(vendor_id)s;
+            WHERE ads.id = %(ad_id)s;
         '''
         return connectToMySQL(cls.DB).query_db(query, data)

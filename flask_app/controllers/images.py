128c6995840d93c320a06f4a73e3473158261ae5
Ruslan Pashayev <92914820+rpashayev@users.noreply.github.com>
2023-06-27 09:07:21 -0500

    Create images.py

---
+++ flask_app/controllers/images.py
@@ -0,0 +1,17 @@
+from flask_app import app
+from flask import render_template, redirect, request, session, flash
+from flask_app.models import category, message, user, vendor, ad, review, image
+
+@app.route('/images/add', methods = ['POST'])
+def add_image():
+    if 'id' not in session:
+        return redirect('/')
+    data = {
+        'image_id':image.Image.upload_file(request.form['image_path']),
+        'vendor_id': session['id']
+    }
+    if not ad.Ad.validate_file(request.files['image_path'].filename):
+        return redirect('/vendors/account')
+    image.Image.add_image(data)
+  
+    return redirect('/vendors/account')
